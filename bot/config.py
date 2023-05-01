742bbbf6fa25633abd2b2ab8f152fcd80e695807
Pedro Gruvhagen <pedro@gruvhagen.com>
2023-05-01 15:58:02 +0200

    Pedro

--- bot/config.py
+++ bot/config.py
@@ -4,14 +4,14 @@ from pathlib import Path
 
 config_dir = Path(__file__).parent.parent.resolve() / "config"
 
-# load yaml config
-with open(config_dir / "config.yml", 'r') as f:
+# Load yaml config
+with open(config_dir / "config.yml", 'r', encoding='utf-8') as f:
     config_yaml = yaml.safe_load(f)
 
-# load .env config
+# Load .env config
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
-# config parameters
+# Config parameters
 telegram_token = config_yaml["telegram_token"]
 openai_api_key = config_yaml["openai_api_key"]
 use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
@@ -20,15 +20,15 @@ new_dialog_timeout = config_yaml["new_dialog_timeout"]
 enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
-mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
+mssql_connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={config_env['MSSQL_HOST']};DATABASE={config_env['MSSQL_DATABASE']};UID={config_env['MSSQL_USER']};PWD={config_env['MSSQL_PASSWORD']}"
 
-# chat_modes
-with open(config_dir / "chat_modes.yml", 'r') as f:
+# Chat_modes
+with open(config_dir / "chat_modes.yml", 'r', encoding='utf-8') as f:
     chat_modes = yaml.safe_load(f)
 
-# models
-with open(config_dir / "models.yml", 'r') as f:
+# Models
+with open(config_dir / "models.yml", 'r', encoding='utf-8') as f:
     models = yaml.safe_load(f)
 
-# files
+# Files
 help_group_chat_video_path = Path(__file__).parent.parent.resolve() / "static" / "help_group_chat.mp4"
--- bot/database.py
+++ bot/database.py
@@ -1,67 +1,66 @@
 from typing import Optional, Any
-
-import pymongo
 import uuid
+import pymssql
 from datetime import datetime
-
-import config
-
 
 class Database:
     def __init__(self):
-        self.client = pymongo.MongoClient(config.mongodb_uri)
-        self.db = self.client["chatgpt_telegram_bot"]
-
-        self.user_collection = self.db["user"]
-        self.dialog_collection = self.db["dialog"]
+        mssql_connection_string = {
+            "host": "PEDROSPC",
+            "user": "pedrogpt",
+            "password": "@Karoline9106040182",
+            "database": "master"
+        }
+        self.conn = pymssql.connect(**mssql_connection_string)
 
     def check_if_user_exists(self, user_id: int, raise_exception: bool = False):
-        if self.user_collection.count_documents({"_id": user_id}) > 0:
-            return True
-        else:
-            if raise_exception:
-                raise ValueError(f"User {user_id} does not exist")
+        with self.conn.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM [user] WHERE id = %s", (user_id,))
+            count = cursor.fetchone()[0]
+            if count > 0:
+                return True
             else:
-                return False
+                if raise_exception:
+                    raise ValueError(f"User {user_id} does not exist")
+                else:
+                    return False
 
-    def add_new_user(
-        self,
-        user_id: int,
-        chat_id: int,
-        username: str = "",
-        first_name: str = "",
-        last_name: str = "",
-    ):
-        user_dict = {
-            "_id": user_id,
+    def add_new_user(self, user_id: int, chat_id: int, username: str = "", first_name: str = "", last_name: str = ""):
+        user = {
+            "id": user_id,
             "chat_id": chat_id,
-
             "username": username,
             "first_name": first_name,
             "last_name": last_name,
-
             "last_interaction": datetime.now(),
             "first_seen": datetime.now(),
-
             "current_dialog_id": None,
             "current_chat_mode": "assistant",
-            "current_model": config.models["available_text_models"][0],
-
+            "current_model": "default_model",
             "n_used_tokens": {},
-
             "n_generated_images": 0,
-            "n_transcribed_seconds": 0.0  # voice message transcription
+            "n_transcribed_seconds": 0.0
         }
 
         if not self.check_if_user_exists(user_id):
-            self.user_collection.insert_one(user_dict)
+            with self.conn.cursor() as cursor:
+                cursor.execute("""
+                INSERT INTO [user] (id, chat_id, username, first_name, last_name,
+                                  last_interaction, first_seen, current_dialog_id,
+                                  current_chat_mode, current_model, n_used_tokens,
+                                  n_generated_images, n_transcribed_seconds)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """, (user["id"], user["chat_id"], user["username"], user["first_name"], user["last_name"],
+                      user["last_interaction"], user["first_seen"], user["current_dialog_id"], user["current_chat_mode"], user["current_model"], str(user["n_used_tokens"]),
+                      user["n_generated_images"], user["n_transcribed_seconds"]))
+                self.conn.commit()
 
     def start_new_dialog(self, user_id: int):
         self.check_if_user_exists(user_id, raise_exception=True)
 
         dialog_id = str(uuid.uuid4())
-        dialog_dict = {
-            "_id": dialog_id,
+        dialog = {
+            "id": dialog_id,
             "user_id": user_id,
             "chat_mode": self.get_user_attribute(user_id, "current_chat_mode"),
             "start_time": datetime.now(),
@@ -69,29 +68,32 @@ class Database:
             "messages": []
         }
 
-        # add new dialog
-        self.dialog_collection.insert_one(dialog_dict)
+        with self.conn.cursor() as cursor:
+            cursor.execute("""
+            INSERT INTO dialog (id, user_id, chat_mode, start_time, model, messages)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """, (dialog["id"], dialog["user_id"], dialog["chat_mode"], dialog["start_time"], dialog["model"], str(dialog["messages"])))
+            self.conn.commit()
 
-        # update user's current dialog
-        self.user_collection.update_one(
-            {"_id": user_id},
-            {"$set": {"current_dialog_id": dialog_id}}
-        )
+        self.set_user_attribute(user_id, "current_dialog_id", dialog_id)
 
         return dialog_id
 
     def get_user_attribute(self, user_id: int, key: str):
         self.check_if_user_exists(user_id, raise_exception=True)
-        user_dict = self.user_collection.find_one({"_id": user_id})
 
-        if key not in user_dict:
-            return None
+        with self.conn.cursor() as cursor:
+            cursor.execute("SELECT {} FROM [user] WHERE id = %s".format(key), (user_id,))
+            value = cursor.fetchone()[0]
 
-        return user_dict[key]
+        return value
 
     def set_user_attribute(self, user_id: int, key: str, value: Any):
         self.check_if_user_exists(user_id, raise_exception=True)
-        self.user_collection.update_one({"_id": user_id}, {"$set": {key: value}})
+
+        with self.conn.cursor() as cursor:
+            cursor.execute(f"UPDATE [user] SET {key} = %s WHERE id = %s", (value, user_id))
+            self.conn.commit()
 
     def update_n_used_tokens(self, user_id: int, model: str, n_input_tokens: int, n_output_tokens: int):
         n_used_tokens_dict = self.get_user_attribute(user_id, "n_used_tokens")
@@ -113,16 +115,18 @@ class Database:
         if dialog_id is None:
             dialog_id = self.get_user_attribute(user_id, "current_dialog_id")
 
-        dialog_dict = self.dialog_collection.find_one({"_id": dialog_id, "user_id": user_id})
-        return dialog_dict["messages"]
+        with self.conn.cursor() as cursor:
+            cursor.execute("SELECT messages FROM dialog WHERE id = %s AND user_id = %s", (dialog_id, user_id))
+            messages = cursor.fetchone()[0]
 
-    def set_dialog_messages(self, user_id: int, dialog_messages: list, dialog_id: Optional[str] = None):
+        return messages
+
+    def set_dialog_messages(self, user_id: int, dialog_messages: list, dialog_id: Optional[str]=None):
         self.check_if_user_exists(user_id, raise_exception=True)
 
         if dialog_id is None:
             dialog_id = self.get_user_attribute(user_id, "current_dialog_id")
 
-        self.dialog_collection.update_one(
-            {"_id": dialog_id, "user_id": user_id},
-            {"$set": {"messages": dialog_messages}}
-        )
+        with self.conn.cursor() as cursor:
+            cursor.execute("UPDATE dialog SET messages = %s WHERE id = %s AND user_id = %s", (str(dialog_messages), dialog_id, user_id))
+            self.conn.commit()
